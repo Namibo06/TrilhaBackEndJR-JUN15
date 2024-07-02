ca0d38c2cbaf5a2894d6a15ac600d97ee61b9fcd
Namibo06 <namibo654@gmail.com>
2024-07-01 23:56:55 -0300

    refactor: Configuration

--- src/main/java/com/codigoCerto/tarefas/configs/Configuration.java
+++ src/main/java/com/codigoCerto/tarefas/configs/Configuration.java
@@ -1,10 +1,29 @@
 package com.codigoCerto.tarefas.configs;
 
+import org.hibernate.cfg.Environment;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
+    @Autowired Environment env;
+
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Environment.getProperties().getProperty("driverClassName"));
+        dataSource.setUrl(Environment.getProperties().getProperty("url"));
+        dataSource.setUsername(Environment.getProperties().getProperty("username"));
+        dataSource.setPassword(Environment.getProperties().getProperty("password"));
+
+        return dataSource;
     }
 }
