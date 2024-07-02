529713af0b915a53cc276818089859f8c400c948
Namibo06 <namibo654@gmail.com>
2024-07-02 17:48:14 -0300

    feat: getUserById

--- src/main/java/com/codigoCerto/tarefas/controllers/UserController.java
+++ src/main/java/com/codigoCerto/tarefas/controllers/UserController.java
@@ -1,14 +1,12 @@
 package com.codigoCerto.tarefas.controllers;
 
 import com.codigoCerto.tarefas.dtos.ResponseApiMessageStatus;
+import com.codigoCerto.tarefas.dtos.ResponseUserDTO;
 import com.codigoCerto.tarefas.dtos.UserDTO;
 import com.codigoCerto.tarefas.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -21,10 +19,15 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<ResponseApiMessageStatus> createUser(@RequestBody UserDTO userDTO, UriComponentsBuilder uriBuilder){
-        System.out.println(userDTO);
         ResponseApiMessageStatus response = service.createUserService(userDTO);
         URI path = uriBuilder.path("/users/{id}").buildAndExpand(userDTO.getId()).toUri();
 
         return ResponseEntity.created(path).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseUserDTO> getUserById(@PathVariable Long id, @RequestBody ResponseUserDTO userDTO){
+        ResponseUserDTO userById = service.findUserById(id);
+        return ResponseEntity.ok(userById);
     }
 }
