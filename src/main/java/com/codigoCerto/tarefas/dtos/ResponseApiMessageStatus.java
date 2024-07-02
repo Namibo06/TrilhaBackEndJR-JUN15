ff3c80c4120899fbe403707af2c7af45f846c061
Namibo06 <namibo654@gmail.com>
2024-07-02 13:54:27 -0300

    refactor: ResponseApiMessageStatus

--- src/main/java/com/codigoCerto/tarefas/dtos/ResponseApiMessageStatus.java
+++ src/main/java/com/codigoCerto/tarefas/dtos/ResponseApiMessageStatus.java
@@ -1,10 +1,14 @@
 package com.codigoCerto.tarefas.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class ResponseApiMessageStatus {
     private String message;
     private Integer status;
