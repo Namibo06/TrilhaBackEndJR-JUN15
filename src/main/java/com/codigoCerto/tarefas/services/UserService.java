3deaccb025ea1a3ad9f4f6fcbfa3854257e41e31
Namibo06 <namibo654@gmail.com>
2024-07-02 13:55:26 -0300

    refactor: UserService

--- src/main/java/com/codigoCerto/tarefas/services/UserService.java
+++ src/main/java/com/codigoCerto/tarefas/services/UserService.java
@@ -1,8 +1,12 @@
 package com.codigoCerto.tarefas.services;
 
+import com.codigoCerto.tarefas.dtos.ResponseApiMessageStatus;
 import com.codigoCerto.tarefas.dtos.UserDTO;
+import com.codigoCerto.tarefas.models.User;
 import com.codigoCerto.tarefas.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +14,27 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public UserDTO createUserService(UserDTO userDTO){
+    @Autowired
+    private ModelMapper modelMapper;
 
+    public ResponseApiMessageStatus createUserService(UserDTO userDTO){
+        boolean existsByEmail = existsByEmailService(userDTO.getEmail());
+        if (existsByEmail){
+            throw new DataIntegrityViolationException("Email já existe,tente outro por favor");
+        }
+
+        User userModel = modelMapper.map(userDTO, User.class);
+        userModel.setUsername(userDTO.getUsername());
+        userModel.setEmail(userDTO.getEmail());
+        userModel.setPassword(userDTO.getPassword());
+        repository.save(userModel);
+
+        String message = "Usuário criado com sucesso!";
+        Integer status = 201;
+        return new ResponseApiMessageStatus(message,status);
+    }
+
+    public Boolean existsByEmailService(String email){
+        return repository.existsByEmail(email);
     }
 }
