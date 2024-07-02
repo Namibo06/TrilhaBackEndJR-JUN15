59bfa7e2433c537b31f495f918e8173480d8defc
Namibo06 <namibo654@gmail.com>
2024-07-02 17:47:37 -0300

    feat: ResponseUserDTO

---
+++ src/main/java/com/codigoCerto/tarefas/dtos/ResponseUserDTO.java
@@ -0,0 +1,11 @@
+package com.codigoCerto.tarefas.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseUserDTO {
+    String username;
+    String email;
+}
