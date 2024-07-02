4695ae18cba06b9bdccd949fed222e1ed656722c
Namibo06 <namibo654@gmail.com>
2024-07-02 13:56:01 -0300

    feat: TestUser

---
+++ src/test/java/com/codigoCerto/tarefas/TestUser.java
@@ -0,0 +1,26 @@
+package com.codigoCerto.tarefas;
+
+import com.codigoCerto.tarefas.dtos.ResponseApiMessageStatus;
+import com.codigoCerto.tarefas.dtos.UserDTO;
+import com.codigoCerto.tarefas.services.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+public class TestUser {
+    @Autowired
+    private UserService service;
+
+    @Test
+    public void testCreateUser(){
+        UserDTO userDTO = new UserDTO(null,"Joca","joca@gmail.com","123",null);
+        ResponseApiMessageStatus response = service.createUserService(userDTO);
+        assertNotNull(response);
+    }
+}
