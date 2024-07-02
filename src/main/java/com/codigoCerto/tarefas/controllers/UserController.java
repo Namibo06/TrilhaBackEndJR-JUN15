c1cc8ecdd032756ad40cc8ba66bd88ff338a8b4f
Namibo06 <namibo654@gmail.com>
2024-07-01 23:31:14 -0300

    feat: UserController

--- src/main/java/com/codigoCerto/tarefas/controllers/UserController.java
+++ src/main/java/com/codigoCerto/tarefas/controllers/UserController.java
@@ -1,9 +1,23 @@
 package com.codigoCerto.tarefas.controllers;
 
+import com.codigoCerto.tarefas.dtos.ResponseApiMessageStatus;
+import com.codigoCerto.tarefas.dtos.UserDTO;
+import com.codigoCerto.tarefas.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService service;
+
+    @PostMapping
+    public ResponseEntity<ResponseApiMessageStatus> createUser(@RequestBody UserDTO userDTO){
+
+    }
 }
