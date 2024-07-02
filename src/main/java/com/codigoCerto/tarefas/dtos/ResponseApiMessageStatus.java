c734948bdcabeab12606ffc51c48ba014922aee4
Namibo06 <namibo654@gmail.com>
2024-07-01 23:29:30 -0300

    feat: ResponseApiMessageStatus

---
+++ src/main/java/com/codigoCerto/tarefas/dtos/ResponseApiMessageStatus.java
@@ -0,0 +1,11 @@
+package com.codigoCerto.tarefas.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseApiMessageStatus {
+    private String message;
+    private Integer status;
+}
