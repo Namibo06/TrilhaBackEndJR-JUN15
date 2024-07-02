331aa46f9aaa7ff343ac2e1ed95337228e9016b0
Namibo06 <namibo654@gmail.com>
2024-07-01 23:31:23 -0300

    feat: UserService

--- src/main/java/com/codigoCerto/tarefas/services/UserService.java
+++ src/main/java/com/codigoCerto/tarefas/services/UserService.java
@@ -1,8 +1,16 @@
 package com.codigoCerto.tarefas.services;
 
+import com.codigoCerto.tarefas.dtos.UserDTO;
+import com.codigoCerto.tarefas.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository repository;
 
+    public UserDTO createUserService(UserDTO userDTO){
+
+    }
 }
