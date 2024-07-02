08e8f0c9b6e6c49f4a3f32930954c626b6c1a579
Namibo06 <namibo654@gmail.com>
2024-07-01 23:29:11 -0300

    refactor: UserDTO

--- src/main/java/com/codigoCerto/tarefas/dtos/UserDTO.java
+++ src/main/java/com/codigoCerto/tarefas/dtos/UserDTO.java
@@ -6,4 +6,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String token;
 }
