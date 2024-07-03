5183683b3d9fdfb6e365946e39da20a446bda034
Namibo06 <namibo654@gmail.com>
2024-07-02 21:39:05 -0300

    refactor: deleteUserByIdService

--- src/main/java/com/codigoCerto/tarefas/services/UserService.java
+++ src/main/java/com/codigoCerto/tarefas/services/UserService.java
@@ -10,6 +10,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
