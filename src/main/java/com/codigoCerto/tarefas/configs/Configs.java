7ab9a6b3291959913d63400f2ccef893f82b8c1a
Namibo06 <namibo654@gmail.com>
2024-07-04 12:43:08 -0300

    refactor: Configs

--- src/main/java/com/codigoCerto/tarefas/configs/Configs.java
+++ src/main/java/com/codigoCerto/tarefas/configs/Configs.java
@@ -3,19 +3,18 @@ package com.codigoCerto.tarefas.configs;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity
+@EnableWebSecurity
 public class Configs {
     @Bean
     public ModelMapper modelMapper(){
@@ -29,13 +28,9 @@ public class Configs {
 
     @Bean
     public SecurityFilterChain filterSecurity(HttpSecurity http) throws Exception{
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeRequests(exchanges -> exchanges
-                        .dispatcherTypeMatchers(HttpMethod.valueOf("/users/**")).permitAll() // Rota de usuários como pública
-                        .anyRequest().authenticated()
-                )
-                .build();
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
     }
 
     @Bean
