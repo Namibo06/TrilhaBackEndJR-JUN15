267845c5c60be3aca0d7949444d76d749dbcdc4c
Namibo06 <namibo654@gmail.com>
2024-07-04 14:39:28 -0300

    feat: TaskController | createTask

---
+++ src/main/java/com/codigoCerto/tarefas/controllers/TaskController.java
@@ -0,0 +1,31 @@
+package com.codigoCerto.tarefas.controllers;
+
+import com.codigoCerto.tarefas.dtos.ResponseApiMessageStatus;
+import com.codigoCerto.tarefas.dtos.TaskDTO;
+import com.codigoCerto.tarefas.services.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    @Autowired
+    private TaskService service;
+
+    @PostMapping
+    public ResponseEntity<ResponseApiMessageStatus> createTask(@RequestBody @Valid TaskDTO taskDTO, UriComponentsBuilder uriBuilder){
+        ResponseApiMessageStatus response = service.createTaskService(taskDTO);
+        URI path = uriBuilder.path("/tasks/{id}").buildAndExpand(taskDTO.getId()).toUri();
+
+        return ResponseEntity.created(path).body(response);
+    }
+}
