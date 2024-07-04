04b3f5d80c94135aa787b5a29c7f648032b64db5
Namibo06 <namibo654@gmail.com>
2024-07-04 02:10:20 -0300

    refactor: filterSecurity

--- src/main/java/com/codigoCerto/tarefas/configs/Configs.java
+++ src/main/java/com/codigoCerto/tarefas/configs/Configs.java
@@ -3,18 +3,19 @@ package com.codigoCerto.tarefas.configs;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class Configs {
     @Bean
     public ModelMapper modelMapper(){
@@ -28,9 +29,13 @@ public class Configs {
 
     @Bean
     public SecurityFilterChain filterSecurity(HttpSecurity http) throws Exception{
-        http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return http.build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(exchanges -> exchanges
+                        .dispatcherTypeMatchers(HttpMethod.valueOf("/users/**")).permitAll() // Rota de usuários como pública
+                        .anyRequest().authenticated()
+                )
+                .build();
     }
 
     @Bean
