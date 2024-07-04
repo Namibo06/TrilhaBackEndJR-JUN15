ca78bdcc71d128c10006d020c14a111d304088e5
Namibo06 <namibo654@gmail.com>
2024-07-04 15:04:53 -0300

    feat: findAllTasks

--- src/main/java/com/codigoCerto/tarefas/controllers/TaskController.java
+++ src/main/java/com/codigoCerto/tarefas/controllers/TaskController.java
@@ -6,11 +6,11 @@ import com.codigoCerto.tarefas.services.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -27,5 +27,12 @@ public class TaskController {
         URI path = uriBuilder.path("/tasks/{id}").buildAndExpand(taskDTO.getId()).toUri();
 
         return ResponseEntity.created(path).body(response);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<TaskDTO>> findAllTasks(@PageableDefault(size = 15) Pageable pageable){
+        Page<TaskDTO> pageableTasks =  service.findAllTasksService(pageable);
+
+        return ResponseEntity.ok(pageableTasks);
     }
 }
