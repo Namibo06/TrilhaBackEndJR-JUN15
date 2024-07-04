a26033008f3113e45b06081e6312092886b9edff
Namibo06 <namibo654@gmail.com>
2024-07-04 00:57:23 -0300

    refactor: login

--- src/main/java/com/codigoCerto/tarefas/controllers/LoginController.java
+++ src/main/java/com/codigoCerto/tarefas/controllers/LoginController.java
@@ -5,6 +5,7 @@ import com.codigoCerto.tarefas.dtos.ResponseApiMessageStatus;
 import com.codigoCerto.tarefas.dtos.UserDTO;
 import com.codigoCerto.tarefas.services.LoginService;
 import com.codigoCerto.tarefas.services.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -29,8 +30,10 @@ public class LoginController {
 
     @PostMapping
     public ResponseEntity<ResponseApiMessageStatus> login(@RequestBody UserDTO userDTO){
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDTO.getEmail(),userDTO.getPassword());
-        Authentication authentication=authenticate.authenticate(token);
+        Boolean existsUser = userService.authenticateUser(userDTO.getEmail(),userDTO.getPassword());
+        if(!existsUser){
+            throw new EntityNotFoundException("Usuário não existe");
+        }
         String tokenCreated = loginService.createToken();
         ResponseApiMessageStatus updateUserTokenById = userService.updateTokenById(userDTO.getId(),tokenCreated);
 
