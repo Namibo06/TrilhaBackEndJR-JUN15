029aa773bc585c56dc80e89a5fa5b331724c76ac
Namibo06 <namibo654@gmail.com>
2024-07-04 01:19:10 -0300

    feat: findByEmail

--- src/main/java/com/codigoCerto/tarefas/repositories/UserRepository.java
+++ src/main/java/com/codigoCerto/tarefas/repositories/UserRepository.java
@@ -4,9 +4,12 @@ import com.codigoCerto.tarefas.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     Boolean existsByEmailAndPassword(String email,String password);
 }
