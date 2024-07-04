4cbabfb5d360cacce43e4ccc2ec9a90344ca28f6
Namibo06 <namibo654@gmail.com>
2024-07-04 14:37:36 -0300

    feat: Task

---
+++ src/main/java/com/codigoCerto/tarefas/models/Task.java
@@ -0,0 +1,27 @@
+package com.codigoCerto.tarefas.models;
+
+import com.codigoCerto.tarefas.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_task")
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 150,nullable = false)
+    private String title;
+    private String description;
+    @Column(nullable = false)
+    private Status status;
+    @Column(nullable = false)
+    private Long userId;
+}
