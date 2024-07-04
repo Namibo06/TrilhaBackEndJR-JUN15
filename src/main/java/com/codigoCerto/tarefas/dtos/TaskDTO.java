1ec40f43be87640e32bdf635dfae9aa48fada846
Namibo06 <namibo654@gmail.com>
2024-07-04 14:38:12 -0300

    feat: TaskDTO

---
+++ src/main/java/com/codigoCerto/tarefas/dtos/TaskDTO.java
@@ -0,0 +1,21 @@
+package com.codigoCerto.tarefas.dtos;
+
+import com.codigoCerto.tarefas.enums.Status;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TaskDTO {
+    private Long id;
+    @NonNull
+    @NotBlank
+    private String title;
+    private String description;
+    @NonNull
+    private Status status;
+    @NonNull
+    private Long userId;
+}
