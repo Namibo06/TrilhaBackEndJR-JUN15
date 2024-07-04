af8e4106aee931eb5c8eef559e19853013ba4785
Namibo06 <namibo654@gmail.com>
2024-07-04 17:49:29 -0300

    feat: verifyToken

--- src/main/java/com/codigoCerto/tarefas/services/LoginService.java
+++ src/main/java/com/codigoCerto/tarefas/services/LoginService.java
@@ -1,8 +1,11 @@
 package com.codigoCerto.tarefas.services;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.codigoCerto.tarefas.dtos.LoginDTO;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +25,20 @@ public class LoginService {
                     .sign(algorithm);
         }catch (JWTCreationException e){
             throw new RuntimeException("Falha ao criar token");
+        }
+    }
+
+    public void verifyToken(String token){
+        DecodedJWT decodedJWT;
+        try{
+            Algorithm algorithm = Algorithm.HMAC256("CodigoCertoTrilhaBackEndJr");
+            JWTVerifier verifier=JWT.require(algorithm)
+                    .withIssuer("Codigo Certo")
+                    .build();
+
+            decodedJWT = verifier.verify(token);
+        }catch (JWTVerificationException e){
+            throw new JWTVerificationException("Falha ao autenticar token: ",e);
         }
     }
 }
