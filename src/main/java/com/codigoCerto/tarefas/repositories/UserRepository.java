5f7e747584a5fba24f6c5214b23bba3ad37a3e1f
Namibo06 <namibo654@gmail.com>
2024-07-04 00:56:50 -0300

    feat: existsByEmailAndPassword

--- src/main/java/com/codigoCerto/tarefas/repositories/UserRepository.java
+++ src/main/java/com/codigoCerto/tarefas/repositories/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Boolean existsByEmail(String email);
+
+    Boolean existsByEmailAndPassword(String email,String password);
 }
