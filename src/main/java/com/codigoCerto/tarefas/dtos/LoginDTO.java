ee2be408ba642094b6e2e86fb2ef09c6bb76bc0e
Namibo06 <namibo654@gmail.com>
2024-07-04 00:32:09 -0300

    feat: LoginDTO

--- src/main/java/com/codigoCerto/tarefas/dtos/LoginDTO.java
+++ src/main/java/com/codigoCerto/tarefas/dtos/LoginDTO.java
@@ -1,10 +1,14 @@
 package com.codigoCerto.tarefas.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class LoginDTO {
     String token;
     String message;
