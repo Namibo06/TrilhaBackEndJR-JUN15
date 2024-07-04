23e922a07b26d8d2fddb136c0a1c20713fcc0167
Namibo06 <namibo654@gmail.com>
2024-07-04 13:04:58 -0300

    feat: ResponseTokenDTO

---
+++ src/main/java/com/codigoCerto/tarefas/dtos/ResponseTokenDTO.java
@@ -0,0 +1,16 @@
+package com.codigoCerto.tarefas.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class ResponseTokenDTO {
+    String token;
+    String message;
+    Integer status;
+}
