f808e5ea899df7480b42f42a8c164bab6603f61d
Namibo06 <namibo654@gmail.com>
2024-07-04 00:31:50 -0300

    feat: AuthenticationManager

--- src/main/java/com/codigoCerto/tarefas/configs/Configs.java
+++ src/main/java/com/codigoCerto/tarefas/configs/Configs.java
@@ -3,11 +3,18 @@ package com.codigoCerto.tarefas.configs;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
 public class Configs {
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
