4eb6212939057bcf35b7cdfc2c844d74794dae1c
Namibo06 <namibo654@gmail.com>
2024-07-19 20:16:48 -0300

    feat: ErrorResponseDTO

---
+++ src/main/java/com/codigoCerto/tarefas/dtos/ErrorResponseDTO.java
@@ -0,0 +1,15 @@
+package com.codigoCerto.tarefas.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class ErrorResponseDTO {
+    String message;
+    String details;
+}
