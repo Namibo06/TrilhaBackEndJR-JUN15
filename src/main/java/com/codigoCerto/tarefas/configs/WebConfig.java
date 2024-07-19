ba89e327fb8bf3d3bde8861e257d995034645c35
Namibo06 <namibo654@gmail.com>
2024-07-19 15:16:58 -0300

    uncommented

--- src/main/java/com/codigoCerto/tarefas/configs/WebConfig.java
+++ src/main/java/com/codigoCerto/tarefas/configs/WebConfig.java
@@ -1,7 +1,7 @@
 package com.codigoCerto.tarefas.configs;
 
 
-/*import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,9 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://trilhabackendjr-jun15-production-e24a.up.railway.app","http://localhost:8080")
+                .allowedOrigins("http://trilhabackendjr-jun15-production-e24a.up.railway.app/","https://trilhabackendjr-jun15-production-e24a.up.railway.app","http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-}*/
+}
