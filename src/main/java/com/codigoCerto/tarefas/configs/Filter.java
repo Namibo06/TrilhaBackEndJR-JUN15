8191d16c57b90114e4b2a16c572968b1fe3c5f33
Namibo06 <namibo654@gmail.com>
2024-07-19 00:46:20 -0300

    refactor: Filter

--- src/main/java/com/codigoCerto/tarefas/configs/Filter.java
+++ src/main/java/com/codigoCerto/tarefas/configs/Filter.java
@@ -4,9 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -19,14 +16,16 @@ public class Filter extends OncePerRequestFilter {
         String requestURI = request.getRequestURI();
         String method = request.getMethod();
         System.out.println("Request URI: " + requestURI + " | HTTP Method: " + method);
-        if("/users".equals(requestURI) && "POST".equalsIgnoreCase(method) ||
-        "/login".equals(requestURI ) ||
+
+        if (("/users".equals(requestURI) && "POST".equalsIgnoreCase(method)) ||
+                "/login".equals(requestURI) ||
                 requestURI.startsWith("/swagger-ui/") ||
                 requestURI.startsWith("/v3/api-docs") ||
-                requestURI.startsWith("/favicon.ico")){
-            filterChain.doFilter(request,response);
+                requestURI.startsWith("/favicon.ico")) {
+            filterChain.doFilter(request, response);
             return;
         }
+
         try {
             var token = findToken(request);
             if (token == null || token.isEmpty()) {
@@ -34,22 +33,20 @@ public class Filter extends OncePerRequestFilter {
                 response.getWriter().write("Token nao encontrado");
                 return;
             }
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.getWriter().write(e.getMessage());
             return;
         }
-
-
 
         filterChain.doFilter(request, response);
     }
 
     private String findToken(HttpServletRequest request) {
         var authorization = request.getHeader("Authorization");
-        if (authorization == null || !authorization.startsWith("Bearer ")){
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
             throw new RuntimeException("Token nao encontrado");
         }
-        return  authorization.replace("Bearer","").trim();
+        return authorization.replace("Bearer ", "").trim();
     }
 }
