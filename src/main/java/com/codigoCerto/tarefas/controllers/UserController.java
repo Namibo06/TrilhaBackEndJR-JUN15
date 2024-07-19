041c389ecc0a43afb2a61bd482055a95489ace40
Namibo06 <namibo654@gmail.com>
2024-07-19 03:02:55 -0300

    test

--- src/main/java/com/codigoCerto/tarefas/controllers/UserController.java
+++ src/main/java/com/codigoCerto/tarefas/controllers/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @PostMapping("createUser")
+    @PostMapping("createUserEnd")
     public ResponseEntity<ResponseApiMessageStatus> createUser(@RequestBody UserDTO userDTO, UriComponentsBuilder uriBuilder){
         ResponseApiMessageStatus response = service.createUserService(userDTO);
         URI path = uriBuilder.path("/users/{id}").buildAndExpand(userDTO.getId()).toUri();
